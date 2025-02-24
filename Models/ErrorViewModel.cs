9bb35aa0c52fc89d48e009eed5b367e3af800290
nguyen7925 <nguyenchinguyen7925@gmail.com>
2025-02-24 23:07:43 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DuAnTN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
