e3d8283486e694ab1f563ecc53cad0f66f75486f
nguyen7925 <nguyenchinguyen7925@gmail.com>
2025-02-25 14:49:09 +0700

    Giao Dien

---
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DuAnTN.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
